fc68b681f87b3109e0f3829a3575fa72d6091106
zaki1993 <zaki1993@abv.bg>
2017-02-07 16:53:38 +0200

    Change repository for ITTallents tasks

---
+++ Homework02/Task3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Task3 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		for(int i = -10;i<=10;i++){
+			if(x % 2 != 0){
+				System.out.println(i);
+			}
+		}
+	}
+
+}
